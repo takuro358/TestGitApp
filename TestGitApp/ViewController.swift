b5935e3f1f433f25635017088afbabe5e333bf9a
takuro358 <63958348+takuro358@users.noreply.github.com>
2020-07-07 21:09:03 +0900

    TestGitApp用のRepository作成

--- TestGitApp/ViewController.swift
+++ TestGitApp/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("TestGitApp")
+        
         // Do any additional setup after loading the view.
     }
 
