81112fbe6e023453d1eb16f567f97c8c05836502
takuro358 <63958348+takuro358@users.noreply.github.com>
2020-07-07 20:33:14 +0900

    Initial Commit

---
+++ TestGitApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestGitApp
+//
+//  Created by takuro on 2020/07/07.
+//  Copyright © 2020 takuro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
